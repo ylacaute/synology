37fc5b6a70fbb253ffcb6420abe526452b3508e8
ylacaute <yannick.lacaute@gmail.com>
2017-01-07 14:18:47 +0100

    first commit

---
+++ ssl/renewFreeCertificate.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+
+# This scripts allow to automatically renew the free SSL certificate from Let's Encrypt.
+# Please see this article for more information : 
+
+ACME_DIR=/volume1/etc/cert/acme-tiny
+CERT_DIR=/volume1/etc/cert
+WEB_ROOT_DIR=/volume1/web
+SENDER=root@thorpora.fr
+DEST=yannick.lacaute@gmail.com
+
+
+
+function printError {
+    (>&2 echo "$1")  
+}
+
+
+function sendSuccessEmail {
+  sendmail  -F "Synology Station" -f $SENDER -t $DEST << EOF
+Subject: Synology certificate renewed
+
+  Renewed on $(date)
+
+EOF
+}
+
+
+function sendFailEmail {
+  local errorMsg="$1"
+
+  printError "$errorMsg"
+
+  sendmail  -F "Synology Station" -f $SENDER -t $DEST << EOF
+Subject: Synology certificate FAILED to renew
+
+  Failed on $(date)
+  Error message : $errorMsg
+
+EOF
+}
+
+
+function renewCertificate {
+  cd "$ACME_DIR"
+   
+  python acme_tiny.py --account-key "$CERT_DIR/account.key" --csr "$CERT_DIR/thorpora.csr" --acme-dir "$WEB_ROOT_DIR/.well-known/acme-challenge" > "$CERT_DIR/signed.crt"
+  if [ $? -ne 0 ]; then
+    errorMsg="Certificate renew FAILED, acme_tiny failed to execute"
+    sendFailEmail errorMsg
+    exit 1
+  fi
+
+  wget -O - https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem > "$CERT_DIR/intermediate.pem"
+  if [ $? -ne 0 ]; then
+    errorMsg="Certificate renew FAILED, impossible to retrieve signed.pem from letsencrypt.org"
+    sendFailEmail errorMsg
+    exit 2
+  fi
+
+  cat "$CERT_DIR/signed.crt" "$CERT_DIR/intermediate.pem" > "$CERT_DIR/chained.pem"
+  if [ $? -ne 0 ]; then
+    errorMsg="Certificate renew FAILED, can't generate final certificate"
+    sendFailEmail errorMsg
+    exit 3
+  fi
+
+  nginx -s reload
+  
+}
+
+
+renewCertificate
